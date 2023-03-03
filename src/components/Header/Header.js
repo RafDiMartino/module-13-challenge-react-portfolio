0c7109bccb170a4659d7348684dac4ee755c98ca
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-03-03 14:39:22 +0000

    adding links to navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 import "./Header.css"
 function Header() {
   return (
-    <div className='header'>
+    <header>
+      <nav>
         <ul>
-            <li>Homepage</li>
-            <li>Projects</li>
-            <li>Contact</li>
+          <li><NavLink to='module-13-challenge-react-portfolio/'>Homepage</NavLink></li>
+          <li><NavLink to='module-13-challenge-react-portfolio/projects'>Projects</NavLink></li>
+          <li><NavLink to='module-13-challenge-react-portfolio/contact'>Contact</NavLink></li>
         </ul>
-    </div>
+      </nav>
+    </header>
   )
 }
 
