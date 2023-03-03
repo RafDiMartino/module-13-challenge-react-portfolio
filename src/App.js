4d375666f0a58c378971eefbc1e554ed1588fcf0
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-03-03 13:20:23 +0000

    adding basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Contact from './pages/Contact';
+import Homepage from './pages/Homepage';
+import Projects from './pages/Projects';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='module-13-challenge-react-portfolio/' element={<Homepage />} />
+          <Route path='module-13-challenge-react-portfolio/projects' element={<Projects />} />
+          <Route path='module-13-challenge-react-portfolio/contact' element={<Contact />} />
+        </Routes>
+      </Router>
+      
     </div>
+
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+  return (
+    <div className='header'>
+        <ul>
+            <li>Homepage</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
