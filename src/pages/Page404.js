3523ef2bc6b5cac5e369fbfb7a66bea1469e3c3a
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-03-05 23:49:33 +0000

    adding stlying for 404 page

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import "./Page404.css"
 
 function Page404() {
   return (
-    <h1>This page does not exist!!</h1>
+    <div className='page404-container'>
+      <h2>Oops...This page does not exist</h2>
+      <div className='buttons-container'>
+        <NavLink className='button' to='/'>Homepage</NavLink>
+        <NavLink className='button' to='projects'>My projects</NavLink>
+        <NavLink className='button' to='contact'>Contact me</NavLink>
+      </div>
+    </div>
   )
 }
 
