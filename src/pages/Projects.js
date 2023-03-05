cfb819241eebf8e9f7c986271aa7d806b64ba204
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-03-05 15:30:08 +0000

    testing project component prop

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import "./Projects.css"
+import { useState } from 'react'
+import Project from '../components/Project/Project'
+import projects from "../data/projects.json"
+
 
 function Projects() {
+
+  const [project, setproject] = useState(projects);
+
   return (
     <div className='projects-container'>
       <div className='projects-wrapper'>
         <h2>Projects</h2>
+        {projects.map( p => <Project project={p} key={p.id} />)}
       </div>
-      
-
-
     </div>
   )
 }
