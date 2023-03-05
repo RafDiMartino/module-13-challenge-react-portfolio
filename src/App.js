256cd1a5e29f6c554a1a16da93e04c22112fd985
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-03-05 22:24:06 +0000

    adding hushrouter from react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Contact from './pages/Contact';
 import Homepage from './pages/Homepage';
 import Projects from './pages/Projects';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Page404 from './pages/Page404';
 import Footer from './components/Footer/Footer';
 
@@ -14,9 +14,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path='module-13-challenge-react-portfolio/' element={<Homepage />} />
-          <Route path='module-13-challenge-react-portfolio/projects' element={<Projects />} />
-          <Route path='module-13-challenge-react-portfolio/contact' element={<Contact />} />
+          <Route path='/' element={<Homepage />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
           <Route path='*' element={<Page404 />} />
         </Routes>
         <Footer />
