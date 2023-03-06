e89c5c5df64c621889702301e4a50440ae602cf1
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-03-06 17:06:08 +0000

    adding social-media component

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm/ContactForm'
 import "./Contact.css"
+import SocialMedia from '../components/SocialMedia/SocialMedia'
 
 function Contact() {
   return (
     <div className='contact-container'>
       <div className='contact-wrapper'>
         <h2>Contact me</h2>
+        <p>You can contact me through the links below or send me a message through the form</p>
+        <div className='social-media'>
+          <SocialMedia />
+        </div>
         <div className='form-wrapper'>
           <ContactForm />
         </div>
