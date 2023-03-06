21fa2d1f940532cfa3b349fb1d5c0fad89bab698
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-03-06 15:19:38 +0000

    adding Web3Forms to get content form submitted to my email address

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,10 +4,13 @@ import "./ContactForm.css"
 
 function ContactForm() {
 
+    const [success, setSuccess] = useState(false)
+
     const [formData, setFormData] = useState({
         name: '',
         email: '',
-        message: ''
+        message: '',
+        access_key: '8f7cd656-fec0-4e74-8279-c70496d238cf'
     })
 
     const handleChange = (event) => {
@@ -19,17 +22,41 @@ function ContactForm() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(formData)
+        
+        const data = JSON.stringify(formData);
+
+        fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: data
+        }).then(res => res.json())
+            .then(data => {
+                setSuccess(true)
+                setFormData({
+                    ...formData,
+                    name: '',
+                    email: '',
+                    message: ''
+                })
+                setTimeout(() => {
+                    setSuccess(false)
+                }, 2000);
+            })
+            .catch(err => console.log(err))
     }
 
     return (
         <div className='contact-form-container'>
             <form onSubmit={handleSubmit}>
-                <input name='name' onChange={handleChange} type='text' placeholder='Enter your Name' />
-                <input name='email' onChange={handleChange} type='text' placeholder='Enter your Email Address' />
-                <textarea name='message' onChange={handleChange} placeholder='Enter your message' cols='30' rows='10'></textarea>
+                <input name='name' value={formData.name} onChange={handleChange} type='text' placeholder='Enter your Name' />
+                <input name='email' value={formData.email} onChange={handleChange} type='text' placeholder='Enter your Email Address' />
+                <textarea name='message' value={formData.message} onChange={handleChange} placeholder='Enter your message' cols='30' rows='10'></textarea>
                 <button className='button' type='submit'>Submit</button>
-            </form> 
+            </form>
+            {success && <p className='success-message'>Form submitted successfully!</p>}
         </div>
     )
 }
